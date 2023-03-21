d89edb7aa9d5b5b640f1c44ee0a278a59cd618a7
cauan <cauanbarros14@gmail.com>
2023-03-21 17:31:11 -0300

    req01

---
+++ tests/unit/services/productService.js
@@ -0,0 +1,14 @@
+/* const ProductService = require('../../services/productService');
+const ProductModel = require('../../models/productModel');
+
+jest.mock('../../models/productModel');
+
+describe('ProductService', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('getAllProducts', () => {
+    it()
+  })
+}) */
