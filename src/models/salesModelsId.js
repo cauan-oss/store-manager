6f1759a08190809040b5d0bd94b49f219def006a
cauan <cauanbarros14@gmail.com>
2023-03-29 17:13:06 -0300

    agora sim lint corrigido

--- src/models/salesModelsId.js
+++ src/models/salesModelsId.js
@@ -8,13 +8,17 @@ const registerSales = async () => {
 
 // Req08 
 const listSalesComplete = async () => {
-  const query = 'select sale_Id as saleId, date, product_id as productId, quantity from StoreManager.sales inner join StoreManager.sales_products on id = sale_id;';
+  const query = `select sale_Id as saleId, date, 
+  product_id as productId, quantity from StoreManager.sales 
+  inner join StoreManager.sales_products on id = sale_id;`;
   const [getData] = await connection.execute(query);
   return getData;
 };
 
 const getListIdModel = async (id) => {
-  const query = 'select date, product_id as productId, quantity from StoreManager.sales inner join StoreManager.sales_products on id = sale_id where id = ?';
+  const query = `select date, product_id as productId, 
+  quantity from StoreManager.sales inner join
+  StoreManager.sales_products on id = sale_id where id = ?`;
   const [getId] = await connection.execute(query, [id]);
   return getId;
 };
