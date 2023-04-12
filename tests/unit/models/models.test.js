4172f66b69675491da6bf022670ddc4b5ac8505c
cauan <cauanbarros14@gmail.com>
2023-04-12 18:36:15 -0300

    requisito2

--- tests/unit/models/models.test.js
+++ tests/unit/models/models.test.js
@@ -5,27 +5,27 @@ const { expect } = chai;
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 chai.use(sinonChai)
-const { productAll } = require('./mocks/product.model.mock');
+const { productsAll } = require('./mocks/product.model.mock');
 const productModel = require('../../../src/models/productModels')
 const connection = require('../../../src/models/connection');
 
 describe("testando a camada model", function () {
   it('recuperando uma lista com todos os produtos', async function () {
     // arrange
-    sinon.stub(connection, 'execute').resolves([productAll])
+    sinon.stub(connection, 'execute').resolves([productsAll])
     //act
     const result = await productModel.getAll();
 
     // assert
-    expect(result).to.be.equal(productAll);
+    expect(result).to.be.equal(productsAll);
   })
 
   it('recuperando um produto a partir do seu id', async function () {
-    sinon.stub(connection, 'execute').resolves([[productAll[0]]]);
+    sinon.stub(connection, 'execute').resolves([[productsAll[0]]]);
 
     const result = await productModel.getById(1)
 
-    expect(result).to.be.equal(productAll[0]);
+    expect(result).to.be.equal(productsAll[0]);
   })
   afterEach(function () {
     sinon.restore();
